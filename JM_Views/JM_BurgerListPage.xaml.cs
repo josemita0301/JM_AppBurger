9c2b38b2f5211f76aa784d90d775d0c3441b9db8
Jose Merlo <josemielmer2002@hotmail.com>
2023-01-17 20:40:00 -0500

    Burger register can now be Updated and shown again on appearing items in CollectionView

--- JM_Views/JM_BurgerListPage.xaml.cs
+++ JM_Views/JM_BurgerListPage.xaml.cs
@@ -1,9 +1,8 @@
 namespace JM_AppBurger.JM_Views;
 using JM_AppBurger.JM_Models;
-
 public partial class JM_BurgerListPage : ContentPage
 {
-	public JM_BurgerListPage()
+    public JM_BurgerListPage()
 	{
 		InitializeComponent();
         LoadData();
@@ -32,4 +31,15 @@ public partial class JM_BurgerListPage : ContentPage
         });
     }
 
+    async void JM_CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        JM_Burger jmburger= new JM_Burger();
+        jmburger = burgerList.SelectedItem as JM_Burger;
+
+
+        await Shell.Current.GoToAsync(nameof(JM_BurgerItemPage), true, new Dictionary<string, object>()
+        {
+            ["Item"] = jmburger
+        });
+    }
 }
